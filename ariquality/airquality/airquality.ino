dc73de751dc533ecd6209d061c2be44f0fc6d7ce
EeroVakiparta <eero.vakiparta@student.samk.fi>
2022-03-12 21:01:04 +0200

    basic MQTT

--- ariquality/airquality/airquality.ino
+++ ariquality/airquality/airquality.ino
@@ -1,7 +1,24 @@
-int analogPin = 35;
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "*******";
+const char* password = "*******";
+const char* mqtt_server = "192.168.100.11";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+const int analogPin = 35;
 
 void setup() {
   Serial.begin(115200);
+  delay(1000);
+  WiFi.begin(ssid, password);
+  Serial.println(WiFi.localIP());
+  client.setServer(mqtt_server, 1883);
 }
 
 int getCo2Measurement() {
@@ -19,6 +36,7 @@ int getCo2Measurement() {
 
 void loop() {
   delay(10000);
+  
   float co2measurement = getCo2Measurement();
   if (isnan(co2measurement)) {
     co2measurement = -1;
@@ -27,4 +45,7 @@ void loop() {
 
   Serial.print("ppm: ");
   Serial.println(co2measurement);
+  char co2Sting[8];
+  dtostrf(co2measurement, 1, 2, co2Sting);
+  client.publish("kotiote/airquality", co2Sting);
 }
