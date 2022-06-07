7496e18cf8c5e4ea1ceecf75fe5a2dfbb2f7acd0
EeroVakiparta <eero.vakiparta@student.samk.fi>
2022-06-07 18:40:02 +0300

    Temperature, humidity & pressure(non-work related) BMP280

--- homeoffice/homeoffice/homeoffice.ino
+++ homeoffice/homeoffice/homeoffice.ino
@@ -1,35 +1,78 @@
 /*
-Source: Wisconsin Department of Health Services
-The levels of CO2 in the air and potential health problems are:
+  CO2 AND OTHER VOLATILE GASES
+    The levels of CO2 in the air and potential health problems are:
+  
+    400 ppm: average outdoor air level.
+    400–1,000 ppm: typical level found in occupied spaces with good air exchange.
+    1,000–2,000 ppm: level associated with complaints of drowsiness and poor air.
+    2,000–5,000 ppm: level associated with headaches, sleepiness, and stagnant, stale, stuffy air. Poor concentration, loss of attention, increased heart rate and slight nausea may also be present.
+    5,000 ppm: this indicates unusual air conditions where high levels of other gases could also be present. Toxicity or oxygen deprivation could occur. This is the permissible exposure limit for daily workplace exposures.
+    40,000 ppm: this level is immediately harmful due to oxygen deprivation.
+      Source: Wisconsin Department of Health Services
 
-  400 ppm: average outdoor air level.
-  400–1,000 ppm: typical level found in occupied spaces with good air exchange.
-  1,000–2,000 ppm: level associated with complaints of drowsiness and poor air.
-  2,000–5,000 ppm: level associated with headaches, sleepiness, and stagnant, stale, stuffy air. Poor concentration, loss of attention, increased heart rate and slight nausea may also be present.
-  5,000 ppm: this indicates unusual air conditions where high levels of other gases could also be present. Toxicity or oxygen deprivation could occur. This is the permissible exposure limit for daily workplace exposures.
-  40,000 ppm: this level is immediately harmful due to oxygen deprivation.
- */
+  TEMPERATURE AND HUMIDITY:
+    Summer conditions: optimum temperature of 24.5°C with an acceptable range of 23-26°C
+    Winter conditions: optimum temperature of 22°C with an acceptable range of 20-23.5°C
+      
+    Humidity in relation to acceptable temperatures in office
+    Summer (Light clothing)  
+    If 30% then 24.5 - 28 °C
+    If 60% then 23 - 25.5 °C
+    
+    Winter (Warm clothing) 
+    If 30% then 20.5 - 25.5 °C
+    If 60% then 20 - 24 °C
+  
+    Optimum humidity levels are between 40% and 60%
+    20 to 26 degrees people will become uncomfortable and productivity is likely to drop.
+      Source: Canadian Centre of Occupational Health and Safety CSA Z412-17 Office Ergonomics
+*/
 
 #include "SparkFun_SGP30_Arduino_Library.h"
 #include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_BMP280.h>
 
-SGP30 airquality; 
+SGP30 airquality;
+Adafruit_BMP280 bmp;
 
-void setup() 
-{
+void setup() {
   Serial.begin(9600);
+  while ( !Serial ) delay(100);
+
+  //-- BMP Temperature and humidity sensor setup
+  Serial.println(F("BMP280 test"));
+  unsigned status;
+  status = bmp.begin();
+  if (!status) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+                     "try a different address!"));
+    Serial.print("SensorID was: 0x"); Serial.println(bmp.sensorID(), 16);
+    Serial.print("        ID of 0xFF probably means a bad address\n");
+    Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+    Serial.print("        ID of 0x60 represents a BME 280.\n");
+    while (1) delay(10);
+  }
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+
+  //-- SGP30 CO2 & VOC sensor setup
   Wire.begin();
-  if (airquality.begin() == false){
+  if (airquality.begin() == false) {
     Serial.println("No SGP30 -airquality sensor detected. Problem with wiring?");
     while (1);
   }
   airquality.initAirQuality();
 }
- 
-void loop() 
+
+void loop()
 {
   //SGP30 needs to warm up. It will display wrong values at start (400 ppm TVOC 0 ppb
-  delay(1000); 
+  delay(2000);
+
   airquality.measureAirQuality();
   Serial.print("CO2: ");
   Serial.print(airquality.CO2);
@@ -37,4 +80,19 @@ void loop()
   Serial.print("TVOC:  ");
   Serial.print(airquality.TVOC);
   Serial.println(" ppb");
+
+
+  Serial.print(F("Temperature = "));
+  Serial.print(bmp.readTemperature());
+  Serial.println(" *C");
+
+  Serial.print(F("Pressure = "));
+  Serial.print(bmp.readPressure());
+  Serial.println(" Pa");
+
+  Serial.print(F("Approx altitude = "));
+  Serial.print(bmp.readAltitude(1013.25)); /* TODO: make ability to adjust this to local normal to get best altitude readings */
+  Serial.println(" m");
+
+  Serial.println();
 }
